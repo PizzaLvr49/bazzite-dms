1b826bd144b369b490cdec89133e8869fd884ac8
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2026-05-08 00:47:36 -0400

    Remove quickshell copr enable command

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -7,7 +7,6 @@ dnf5 -y install xdg-desktop-portal-gnome qt6ct
 dnf5 -y install wlsunset cava playerctl brightnessctl
 
 dnf5 -y copr enable avengemedia/dms
-dnf5 -y copr enable outfoxxed/quickshell
 dnf5 -y install niri dms quickshell-git matugen
 
 mkdir -p /usr/lib/systemd/user/niri.service.wants
